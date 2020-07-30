fb986a31e04c90c2f7965f08902100702c6390e3
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-30 13:44:47 -0600

    add all screens, add stacknavigator, switchnavigator, bottomtabnavigator

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <Text>SignupScreen</Text>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
