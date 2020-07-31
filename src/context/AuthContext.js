22963429c6505d2b6fa1ce09bc096b45c53aaf81
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-31 13:36:40 -0600

    error handle in signuo

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,23 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
-
+  return async ({ email, password }) => {
+    try{
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data)
+    } catch (err) {
+      dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'})
+    }
   };
 };
 
@@ -28,5 +36,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signup, signout },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: '' }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -28,9 +30,11 @@ const SignupScreen = ({ navigation }) => {
         autoCorrect={false}
         secureTextEntry
       />
+      {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
       <Spacer>
         <Button
           title="Sign Up"
+          onPress={() => signup({ email, password })}
         />
       </Spacer>
     </View>
@@ -48,6 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 
